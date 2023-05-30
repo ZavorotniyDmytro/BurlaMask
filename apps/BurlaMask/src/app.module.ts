f7d83d41b171580d9af8aade285352c737c92bbf
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-05-30 16:55:35 +0300

    created docker-compose.yml with elastic search
    (~cmd): wsl -d docker-desktop
    (~linux): sysctl -w vm.max_map_count=262144

--- apps/BurlaMask/src/app.module.ts
+++ apps/BurlaMask/src/app.module.ts
@@ -4,8 +4,9 @@ import { ConfigModule } from './config/config.module';
 import { Awss3Module } from './awss3/awss3.module';
 import { PhotoModule } from './photo/photo.module';
 import { ProvidersModule } from '@lib/providers';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [ConfigModule, Awss3Module, PhotoModule, ProvidersModule]
+  imports: [ConfigModule, Awss3Module, PhotoModule, ProvidersModule, SearchModule]
 })
 export class AppModule {}

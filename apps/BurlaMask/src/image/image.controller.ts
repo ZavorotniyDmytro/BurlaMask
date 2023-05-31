42735e3799d405397161329d4d252762c26b630c
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-05-31 14:19:49 +0300

    added endpoints to integrate with AI

--- apps/BurlaMask/src/image/image.controller.ts
+++ apps/BurlaMask/src/image/image.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Body, Controller, Get, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { IDescriptionSearchBody } from '../search/types/descriptionSearchBody.interface';
 import { IDescription } from './dto/description.dto';
 import {ImageService} from './image.service';
@@ -8,14 +8,26 @@ import {ImageService} from './image.service';
 export class ImageController {
 	constructor(private readonly imageService: ImageService){}
 	
-	@Get()
+	@Get('/description')
 	findByPartDescription(@Body() data: IDescription): Promise<IDescriptionSearchBody[]>{
 		return this.imageService.findByPartDescription(data)
 	}
 
 	@Post('/upload')
 	@UseInterceptors(FileInterceptor('file'))
-	create(@UploadedFile() file: Express.Multer.File, @Body() createImageDto: ICreateImageDto){		
+	create(@UploadedFile() file: Express.Multer.File, @Body() createImageDto: ICreateImageDto): Promise<string>{		
 		return this.imageService.create(createImageDto, file)
+	}
+
+	@Post('/swap')
+	@UseInterceptors(
+		FileFieldsInterceptor([
+			{ name: 'images', maxCount: 2 },
+		 ])
+	)
+	async swapFaces(
+		@UploadedFiles() files: { images?: Express.Multer.File[] }
+	) {
+		return this.imageService.swapFaces(files.images[0], files.images[1])
 	}
 }
--- apps/BurlaMask/src/image/image.service.ts
+++ apps/BurlaMask/src/image/image.service.ts
@@ -2,54 +2,101 @@ import { Image } from '@lib/providers/image.entity';
 import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
+import { extname } from 'path';
 import { lastValueFrom } from 'rxjs';
 import { Repository } from 'typeorm';
 import { SearchService } from '../search/search.service';
 import { IDescriptionSearchBody } from '../search/types/descriptionSearchBody.interface';
 import { IDescription } from './dto/description.dto';
 
+export interface ISwappedFaces{
+	image1: Express.Multer.File;
+	image2: Express.Multer.File;
+}
+
 @Injectable()
 export class ImageService {
 	constructor(
-		@Inject('S3_SERVICE') 
+		@Inject('S3_SERVICE')
 		private readonly s3Service: ClientProxy,
-		@InjectRepository(Image) 
+		@InjectRepository(Image)
 		private readonly imageRepository: Repository<Image>,
-		private readonly searchService: SearchService
-	){}
+		private readonly searchService: SearchService,
+	) {}
 
-	async findByPartDescription(data: IDescription): Promise<IDescriptionSearchBody[]>{
-		return await this.searchService.searchByPartDescription(data.description)
+	async findByPartDescription(
+		data: IDescription,
+	): Promise<IDescriptionSearchBody[]> {
+		return await this.searchService.searchByPartDescription(data.description);
 	}
 
-	async create(createImageDto: ICreateImageDto, file: Express.Multer.File): Promise<Image>{
-		const newRecord = { image_url: "", description: createImageDto.description}
-		let image = this.imageRepository.create(newRecord)		
-		image = await this.imageRepository.save(image)
+	async create(
+		createImageDto: ICreateImageDto,
+		file: Express.Multer.File,
+	): Promise<string> {
+		const newRecord = {
+			image_url: '',
+			description: createImageDto.description,
+		};
+		let image = this.imageRepository.create(newRecord);
+		image = await this.imageRepository.save(image);
 
-		file.filename = this.processImageFile(file, image.id)
-		const image_url$ = this.s3Service.send<string, Express.Multer.File>('upload', file)
-		image.image_url = await lastValueFrom(image_url$)				
+		file.filename = this.processImageFile(file, image.id);
+		const image_url$ = this.s3Service.send<string, Express.Multer.File>(
+			'upload',
+			file,
+		);
+		image.image_url = await lastValueFrom(image_url$);
+
+		await this.searchService.indexImage(image);
+		image = await this.imageRepository.save(image);
+		return image.image_url;
+	}
+
+	async swapFaces(image1: Express.Multer.File, image2: Express.Multer.File): Promise<ISwappedFaces>{
+		if (this.isImageFile(image1) && this.isImageFile(image2)){
+
+
+			// обробка
+
+			const images: ISwappedFaces = {image1, image2}
+			return images
+		} else {
+			throw new HttpException(
+				'Wrong file extension. Must be: jpg, jpeg, png',
+				HttpStatus.BAD_REQUEST,
+			);
+		}
+	}
+
+	private processImageFile(
+		file: Express.Multer.File,
+		fileName: string,
+	): string {
 		
-		await this.searchService.indexImage(image)
-		return await this.imageRepository.save(image)
+		if (this.isImageFile(file)) {
+			throw new HttpException(
+			'Wrong file extension. Must be: jpg, jpeg, png',
+			HttpStatus.BAD_REQUEST,
+			);
+		}
+
+		const { size } = file;
+		if (size > 3 * 1024 * 1024) {
+			throw new HttpException(
+			'The image must be no larger than 3 MB',
+			HttpStatus.BAD_REQUEST,
+			);
+		}
+
+		const newFileName = `${fileName}-image.${extname(file.originalname).toLowerCase()}`;
+
+		return newFileName;
 	}
 
-	private processImageFile(file: Express.Multer.File, fileName: string): string {
-		const fileExtension = file.originalname.split('.').pop();
-	 
-		const allowedExtensions = ['jpg', 'jpeg', 'png'];
-		if (!allowedExtensions.includes(fileExtension.toLowerCase())) {
-		  throw new HttpException("Wrong file extension. Must be: jpg, jpeg, png", HttpStatus.BAD_REQUEST, );
-		}
-	 
-		const { size } = file
-		if (size > 3 * 1024 * 1024) {
-			throw new HttpException("The image must be no larger than 3 MB", HttpStatus.BAD_REQUEST);
-		}
-	 
-				const newFileName = `${fileName}-image.${fileExtension}`;
-	 
-		return newFileName;
-	 }
+	private isImageFile(file: Express.Multer.File): boolean {
+		const allowedExtensions = ['.jpg', '.jpeg', '.png'];
+		const fileExtension = extname(file.originalname).toLowerCase();
+		return allowedExtensions.includes(fileExtension);
+	}
 }
